75a1e46a4d1506daa6b07e4ee1a9cb6356cfb798
Armi Mujica <armivolk@gmail.com>
2023-07-05 09:11:35 +0200

    Update Heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Heading = () => {
   return (
     <>
-    <div class="app-title">
-      <span>Armi's Trivia</span>
-    </div>
+      <div class="app-title">
+        <span>Trivia</span>
+      </div>
     </>
   );
-}
+};
 
 export default Heading;
